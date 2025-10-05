9a14f887ea7fe31217efecfa132b530a3fa383b6
Guilherme Poletti <polettigui15@gmail.com>
2025-10-05 10:42:10 -0300

    get list models endpoint

--- specttra-api/app/api/v1/router.py
+++ specttra-api/app/api/v1/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import predict, metrics, train
+from app.api.v1.endpoints import predict, metrics, train, models
 
 router = APIRouter()
 router.include_router(predict.router, tags=["Machine Learning"])
 router.include_router(metrics.router, tags=["Metrics"])
 router.include_router(train.router, tags=["Train"])
+router.include_router(models.router, tags=["Models"])
